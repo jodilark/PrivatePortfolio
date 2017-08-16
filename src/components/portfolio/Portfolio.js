e84f807783321a45453685fc485e9204d0d1ccbf
Jodi Parker <jodilparker@gmail.com>
2017-08-16 09:09:29 -0600

    moved project data to its own file. thinking of redoing the portfolio page to be template based

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import './portfolio.css'
 import $ from 'jquery'
 import { Transition } from 'semantic-ui-react'
 import '../../../node_modules/semantic-ui-css/semantic.min.css'
+import projectList from './projects'
 
 export default class Portfolio extends Component {
     state = { charmiVisible: false, attVisible: false, ffxivVisible: false, tplVisible: false }
@@ -11,46 +12,8 @@ export default class Portfolio extends Component {
     toggleAtt = _ => this.setState({ attVisible: !this.state.attVisible })
     toggleFfxiv = _ => this.setState({ ffxivVisible: !this.state.ffxivVisible })
     toggleTpl = _ => this.setState({ tplVisible: !this.state.tplVisible })
-
     render() {
-        const projects = [
-            {
-                siteName: 'Charmi'
-                , code: 'charmi'
-                , link: 'http://charmi.surge.sh'
-                , github: 'https://github.com/Team-Overkill/Charmi-React'
-                , tech: ['React, ', 'Node, ', 'Express, ', 'SQL, ', 'Axios, ', 'Auth0, ', 'Socket.io, ', 'Redux']
-                , role: 'Full Stack Developer with focus on back-end and data transfer.'
-                , description: 'Developed with 3 other developers over the coarse of a week. Charmi is a dating app similar to tinder. Features include multiple-users, match making, and real-time chat.'
-            },
-            {
-                siteName: 'All The Things'
-                , code: 'att'
-                , link: 'https://allthethngs.com'
-                , github: 'https://github.com/jodilark/allthethings'
-                , tech: ['Angular, ', 'Node, ', 'Express, ', 'SQL, ', 'Quokka Barcode, ', 'Auth0']
-                , role: 'Full Stack Developer'
-                , description: 'An app that allows you to keep a record of all the items you own and where they are stored.'
-            },
-            {
-                siteName: 'Final Fantasy XIV Shopping List'
-                , code: 'ffxivsl'
-                , link: 'http://cute-route.surge.sh/#!/'
-                , github: 'https://github.com/jodilark/noServerProject'
-                , tech: ['Angular, ', 'Node']
-                , role: 'Front-End Developer'
-                , description: 'Add-on application for a video game that allows you to easily search for in-game content.'
-            },
-            {
-                siteName: '3PL Central Warehouse Management System'
-                , code: 'tpl'
-                , link: 'http://2h9nk7.axshare.com/#g=1&p=receiptsfeature&c=1'
-                , github: 'Private'
-                , tech: ['Axure, ', 'Photoshop']
-                , role: 'UX/UI Design'
-                , description: 'Total Redesign of a 2008 based warehouse management software. (Prototype only due to company access restrictions)'
-            }
-        ]
+        const projects = projectList.projectList
         return (
             <div className='portfolio-wrapper' id='webdev'>
                 <div className='type-container webdev'>
